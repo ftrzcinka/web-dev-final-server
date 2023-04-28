f4ecab24946c182b56c033acea345d1d75ce7b2b
ftrzcinka <ftrzcinka@gmail.com>
2023-04-28 14:44:59 -0400

    Remove comment

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const { Sequelize } = require("sequelize");
 const CONNECTION_URL = process.env.CONNECTION_URL;
-// Option 3: Passing parameters separately (other dialects)
+
 module.exports = new Sequelize(CONNECTION_URL);
